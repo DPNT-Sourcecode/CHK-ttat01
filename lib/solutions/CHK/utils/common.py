6b9d08cd68dfb659844d038f3741e50cb2c691cb
sbx_user1051 <sbx_user1051@169.254.55.79>
2025-01-19 18:03:19 +0000

    Sun Jan 19 18:03:19 UTC 2025

--- lib/solutions/CHK/utils/common.py
+++ lib/solutions/CHK/utils/common.py
@@ -1,6 +1,11 @@
+from typing import Optional
 
 
 class Utils:
-    
+
     @staticmethod
-    def generate_constants() -> dict
+    def generate_constants(
+        keys: str, price_range: range, append: Optional[dict] = None
+    ) -> dict:
+        """random sku key (character) - value (price) generator
+        use append argument to apply pre-defined sku and its value"""
