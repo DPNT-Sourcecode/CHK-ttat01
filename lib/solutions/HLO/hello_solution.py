cb1825e943646d6de8b5e9ba5c1657a870d28291
sbx_user1051 <sbx_user1051@169.254.91.119>
2025-01-19 16:57:18 +0000

    Sun Jan 19 16:57:18 UTC 2025

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -1,6 +1,9 @@
-
-
 # noinspection PyUnusedLocal
 # friend_name = unicode string
-def hello(friend_name):
-    raise NotImplementedError()
+def hello(friend_name: str) -> str:
+
+    if not isinstance(friend_name, str):
+        raise TypeError
+
+    return "Hello, {friend_name}".format(friend_name=friend_name)
+
