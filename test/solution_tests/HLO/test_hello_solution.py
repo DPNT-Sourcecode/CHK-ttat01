789369d2934747edfe91ca649445d6f8d4c8b92d
sbx_user1051 <sbx_user1051@169.254.91.119>
2025-01-19 17:06:18 +0000

    Sun Jan 19 17:06:18 UTC 2025

--- test/solution_tests/HLO/test_hello_solution.py
+++ test/solution_tests/HLO/test_hello_solution.py
@@ -5,3 +5,5 @@ from lib.solutions.HLO.hello_solution import hello
 def test_hello_fn():
     assert hello("arif") == "Hello, Arif"
     assert hello(1) == Exception
+    assert hello("arif") != "Hello, World"
+
