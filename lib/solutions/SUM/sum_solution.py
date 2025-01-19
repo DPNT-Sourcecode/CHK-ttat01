c741c10b435784908635235034a7ca1f6c067d0d
sbx_user1051 <sbx_user1051@169.254.11.157>
2025-01-19 16:34:28 +0000

    Sun Jan 19 16:34:28 UTC 2025

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,12 +1,24 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 
+
 class OutOfRange(Exception):
     pass
 
-class Solution:
-    
-    def __init__(self, *, x: int, y: int):
-        
+
+class SumInt:
+
+    VALID_RANGE = range(0, 101)
+
+    def __init__(self, *, x: int, y: int) -> None:
+
+        if not isinstance(x, int) or not isinstance(y, int):
+            raise TypeError("Please enter instance of int")
+
+        if x not in self.VALID_RANGE or y not in self.VALID_RANGE:
+            raise OutOfRange("Please insert integers in a range of 0-100")
+
+        self.x: int = x
+        self.y: int = y
 
 
 def compute(x, y):
@@ -14,4 +26,5 @@ def compute(x, y):
 
     if x not in valid_rage or y not in valid_rage:
         raise ...
+
 
