f29f833ddc9322baeef8e53c8fb9047e005a0fdb
sbx_user1051 <sbx_user1051@169.254.55.79>
2025-01-19 18:06:19 +0000

    Sun Jan 19 18:06:19 UTC 2025

--- lib/solutions/CHK/utils/common.py
+++ lib/solutions/CHK/utils/common.py
@@ -1,4 +1,5 @@
 from typing import Optional
+from random import randint
 
 
 class Utils:
@@ -9,3 +10,9 @@ class Utils:
     ) -> dict:
         """random sku key (character) - value (price) generator
         use append argument to apply pre-defined sku and its value"""
+
+        _start = 10
+        _end = max(price_range)
+
+        return {k: randint(_start, _end) for k in keys}.update(append)
+
