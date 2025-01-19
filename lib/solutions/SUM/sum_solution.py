47c1f5e2fb4c8f5c320ebe298cc28dc0c2ca3aa0
sbx_user1051 <sbx_user1051@169.254.11.157>
2025-01-19 16:31:27 +0000

    Sun Jan 19 16:31:27 UTC 2025

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,17 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
+
+class OutOfRange(Exception):
+    pass
+
+class Solution:
+    
+    def __init__(self, *, x: int, y: int):
+        
+
+
 def compute(x, y):
-    raise NotImplementedError()
+    valid_rage = range(0, 101)
+
+    if x not in valid_rage or y not in valid_rage:
+        raise ...
+
