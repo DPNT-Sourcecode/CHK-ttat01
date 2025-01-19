83a390c6e004f1b1e55a5f4419d291a96ae60507
sbx_user1051 <sbx_user1051@169.254.91.119>
2025-01-19 17:00:18 +0000

    Sun Jan 19 17:00:18 UTC 2025

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -1,9 +1,17 @@
+class EmptyStringError(Exception):
+    pass
+
+
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 def hello(friend_name: str) -> str:
 
     if not isinstance(friend_name, str):
-        raise TypeError
+        raise TypeError("Please enter a string")
 
-    return "Hello, {friend_name}".format(friend_name=friend_name)
+    if len(friend_name) <= 0:
+        raise EmptyStringError("String must not be an empty")
+
+    return "Hello, {friend_name}".format(friend_name=friend_name.capitalize())
+
 
