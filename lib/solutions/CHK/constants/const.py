80806acc75294a4c6bd1d24f79a60f2c146e562e
sbx_user1051 <sbx_user1051@169.254.19.179>
2025-01-20 12:52:56 +0000

    Mon Jan 20 12:52:56 UTC 2025

--- lib/solutions/CHK/constants/const.py
+++ lib/solutions/CHK/constants/const.py
@@ -11,7 +11,34 @@ class Constants:
 
     Deal: type = namedtuple("Deal", ["quantity", "sku_for_free", "count_of_free_sku"])
 
-    EXCLUDE: dict[str, int] = {}
+    EXCLUDE: dict[str, int] = {
+        "A": 50,
+        "B": 30,
+        "C": 20,
+        "D": 15,
+        "E": 40,
+        "F": 10,
+        "G": 20,
+        "H": 10,
+        "I": 35,
+        "J": 60,
+        "K": 80,
+        "L": 90,
+        "M": 15,
+        "N": 40,
+        "O": 10,
+        "P": 50,
+        "Q": 30,
+        "R": 50,
+        "S": 30,
+        "T": 20,
+        "U": 40,
+        "V": 50,
+        "W": 20,
+        "X": 90,
+        "Y": 10,
+        "Z": 50,
+    }
 
     ITEMS: dict[str, int] = Utils.generate_constants(
         keys="GHIJKLMNOPQRSTUVWXYZ", price_range=range(10, 2000), append=EXCLUDE
@@ -20,10 +47,12 @@ class Constants:
     OFFERS: dict[list[Offer]] = {
         "A": [Offer(3, 130), Offer(5, 200)],
         "B": [Offer(2, 45)],
+        "H": [Offer(5, 45), Offer(10, 80)],
     }
 
     DEALS: dict[list[Deal]] = {
         "E": [Deal(quantity=2, sku_for_free="B", count_of_free_sku=1)],
         "F": [Deal(quantity=2, sku_for_free="F", count_of_free_sku=1)],
     }
+
 
