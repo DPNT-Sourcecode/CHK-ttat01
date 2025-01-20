6ef03bbad9ae73c7f9249a3d25534503accb7be8
sbx_user1051 <sbx_user1051@169.254.19.179>
2025-01-20 12:55:56 +0000

    Mon Jan 20 12:55:56 UTC 2025

--- lib/solutions/CHK/constants/const.py
+++ lib/solutions/CHK/constants/const.py
@@ -48,11 +48,19 @@ class Constants:
         "A": [Offer(3, 130), Offer(5, 200)],
         "B": [Offer(2, 45)],
         "H": [Offer(5, 45), Offer(10, 80)],
+        "K": [Offer(2, 150)],
+        "P": [Offer(5, 200)],
+        "Q": [Offer(3, 80)],
+        "V": [Offer(2, 90), Offer(3, 130)],
     }
 
     DEALS: dict[list[Deal]] = {
         "E": [Deal(quantity=2, sku_for_free="B", count_of_free_sku=1)],
         "F": [Deal(quantity=2, sku_for_free="F", count_of_free_sku=1)],
+        "N": [Deal(quantity=3, sku_for_free="M", count_of_free_sku=1)],
+        "R": [Deal(quantity=3, sku_for_free="Q", count_of_free_sku=1)],
+        "U": [Deal(quantity=3, sku_for_free="U", count_of_free_sku=1)],
     }
+
 
 
